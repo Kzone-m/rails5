d1c401ad9ac6789c0a552680779d5a58d7d9ce02
kazune miyagi <kazune.miyagi92@gmail.com>
2017-06-20 00:13:23 +0900

    Add sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
@@ -10,9 +8,9 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
-  get '/signup', to: 'users#new'
-
   get '/canvas', to: 'canvas#canvas'
 
   get '/jump', to: 'canvas#jump'
+
+  resources :users
 end
